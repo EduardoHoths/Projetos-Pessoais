7a7ceb1b475eff0aff42d3b11f2fe316e46e288e
Eduardo <eduardo.hoths@gmail.com>
2022-05-02 21:22:17 -0300

    Fix

--- calculator/src/components/logic/LogicalStorage.jsx
+++ calculator/src/components/logic/LogicalStorage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const LogicalContext = React.createContext();
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const operators = ['+', '-', '*', '/'];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operators = ["+", "-", "*", "/"];
 
 export const LogicalStorage = ({ children }) => {
     const [num, setNum] = React.useState(0);
@@ -13,47 +13,44 @@ export const LogicalStorage = ({ children }) => {
     const [result, setResult] = React.useState(false);
 
     React.useEffect(() => {
-        if(num.toString().includes(".")) {
-            if(num.toString().length >= 10){
-                setNumDisplay(Number(num).toFixed(8).replace('.', ','));
-            } else{
-                setNumDisplay(num.toString().replace('.', ','));
+        if (num.toString().includes(".")) {
+            if (num.toString().length >= 10) {
+                setNumDisplay(Number(num).toFixed(8).replace(".", ","));
+            } else {
+                setNumDisplay(num.toString().replace(".", ","));
             }
-            
         } else {
-            setNumDisplay(num)
+            setNumDisplay(num);
         }
-
-        
     }, [num]);
 
     function handleNumber({ target }) {
-        if(num.toString().length === 13 && !result){
-            return
+        if (num.toString().length === 13 && !result) {
+            return;
         }
-        
-        if(operator && result && !oldNum){ 
-            setOldNum(num)
+
+        if (operator && result && !oldNum) {
+            setOldNum(num);
             setNum(target.innerText);
-            return
+            return;
         }
-        
-        if(result && !operator){
+
+        if (result && !operator) {
             setNum(target.innerText);
-            setResult(false)
-            return
+            setResult(false);
+            return;
         }
-        if (num === 0) {
+        if (num === 0 || num === "0") {
             setNum(target.innerText);
         } else {
             setNum(num + target.innerText);
         }
     }
     function operatorInput({ target }) {
-        if(operator && num !== 0){
-            calculate('')
+        if (operator && num !== 0) {
+            calculate("");
             setOperator(target.innerText);
-            return
+            return;
         }
         setOperator(target.innerText);
         setNum(0);
@@ -67,22 +64,36 @@ export const LogicalStorage = ({ children }) => {
         setNum(0);
         setOldNum(null);
         setOperator(null);
-        setResult(false)
+        setResult(false);
     }
 
     function backspace() {
-        if(num === 0){
-            return 
+        if (num === 0) {
+            return;
         }
-        if(num.toString().length === 1){
-            setNum(0)
-            setResult(false)
-            return
+        if (num.toString().length === 1) {
+            setNum(0);
+            setResult(false);
+            return;
         }
         setNum(num.toString().slice(0, -1));
     }
     function percentage() {
-        setNum(num / 100);
+        if (operator === "+") {
+            setNum(parseFloat(oldNum) + (parseFloat(num) / 100) * parseFloat(oldNum));
+        }
+        if (operator === "-") {
+            setNum(parseFloat(oldNum) - (parseFloat(num) / 100) * parseFloat(oldNum));
+        }
+        if (operator === "x") {
+            setNum(parseFloat(oldNum) * (parseFloat(num) / 100) );
+        }
+        if (operator === "/") {
+            setNum(parseFloat(oldNum) / (parseFloat(num) / 100));
+        }
+        setOperator(null);
+        setOldNum(null);
+        setResult(true)
     }
 
     function decimal() {
@@ -91,85 +102,64 @@ export const LogicalStorage = ({ children }) => {
         }
         setNum(num + ".");
     }
-    function calculate({target}) {
-        if(oldNum === null){
-            return
+    function calculate({ target }) {
+        if (oldNum === null) {
+            return;
         }
         if (operator === "+") {
-            setNum(
-                (
-                    parseFloat(oldNum) +
-                    parseFloat(num)
-                ).toString()
-            );
+            setNum((parseFloat(oldNum) + parseFloat(num)).toString());
             setOldNum(null);
         }
         if (operator === "-") {
-            setNum(
-                (
-                    parseFloat(oldNum) -
-                    parseFloat(num)
-                ).toString()
-            );
+            setNum((parseFloat(oldNum) - parseFloat(num)).toString());
             setOldNum(null);
         }
         if (operator === "x") {
-            setNum(
-                (
-                    parseFloat(oldNum) *
-                    parseFloat(num)
-                ).toString()
-            );
+            setNum((parseFloat(oldNum) * parseFloat(num)).toString());
             setOldNum(null);
         }
         if (operator === "/") {
-            setNum(
-                (
-                    parseFloat(oldNum) /
-                    parseFloat(num)
-                ).toString()
-            );
+            setNum((parseFloat(oldNum) / parseFloat(num)).toString());
             setOldNum(null);
         }
-        
-        setResult(true)
 
-        if(target && target.innerText === '='){
-            setOperator(null)
+        setResult(true);
+
+        if (target && target.innerText === "=") {
+            setOperator(null);
         }
-        
     }
 
-    window.onkeydown = ({key}) => {
-        if(numbers.includes(key)){
-            handleNumber({target: {innerText: key}})
-            return 
+    window.onkeydown = ({ key }) => {
+        if (numbers.includes(key)) {
+            handleNumber({ target: { innerText: key } });
+            return;
         }
-        if(operators.includes(key)){
-            operatorInput({target: {innerText: key}})
-            return 
+        if (operators.includes(key)) {
+            operatorInput({ target: { innerText: key } });
+            return;
         }
-        if(key === ","){
-            decimal()
-            return 
+        if (key === ",") {
+            decimal();
+            return;
         }
-        if(key === "Backspace"){
-            backspace()
-            return 
+        if (key === "Backspace") {
+            backspace();
+            return;
         }
-        if(key === "Delete"){
-            allClear()
-            return 
+        if (key === "Delete") {
+            allClear();
+            return;
         }
-        if(key === "="){
-            calculate({target: {innerText: key}})
-            return 
+        if (key === "=") {
+            calculate({ target: { innerText: key } });
+            return;
         }
-        if(key === "%"){
-            percentage()
-            return 
+        if (key === "%") {
+            percentage();
+            return;
         }
-    }
+    };
 
     return (
         <LogicalContext.Provider
@@ -189,4 +179,3 @@ export const LogicalStorage = ({ children }) => {
         </LogicalContext.Provider>
     );
 };
-
